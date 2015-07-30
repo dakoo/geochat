b1eb2ac0578b98ec05ac27c7c8d82e8970356b7c
Hochul Shin <darkblue99@gmail.com>
2015-07-30 22:58:34 +0900

    The group concept added. a member of User.talkers is a user in a scope.

--- chat_broker/handlers.js
+++ chat_broker/handlers.js
@@ -1,68 +1,124 @@
-var usernames = {};
-var numUsers = 0;
-var addedUser = false;
-var defaultGroupId = "default_group_id";
+var _ = require('underscore');
+var User = function(username, position, radius){
+    this.username = username;
+    this.position = position;
+    this.radius = radius;
+    this.talkers = [];
+}
+var users = [];
+var createTalkerGroup = function(socket, username, position, radius){
+    var user = new User(username, position, radius);
+    if(user.length == 0){
+        socket.join(username);
+        users.push(user);
+        return true;
+    }
+    if(_.findWhere(users, {username:username})!= undefined){
+        return false;
+    }
+    socket.join(username);
+    var talkers = _.filter(users, function(member){
+       var distance = calculateDistance(user.position.latitude,
+       user.position.longitude, member.position.latitude, member.position.longitude);
+        console.log('distance: '+ distance);
+        console.log('user.radius: ' + user.radius);
+        console.log('member.raidus: ' + member.radius);
+        if((user.radius > distance) && (member.radius > distance)){
+            member.talkers.push(username);
+            return true;
+        }
+        else {
+            return false;
+        }
+    });
+    user.talkers = _.pluck(talkers, 'username');
+    users.push(user);
+    return true;
+};
+
+function calculateDistance(lat1, lon1, lat2, lon2) {
+    var R = 6371; // km
+    var dLat = (lat2-lat1).toRad();
+    var dLon = (lon2-lon1).toRad();
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) *
+        Math.sin(dLon/2) * Math.sin(dLon/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c;
+    return d;
+};
+
+Number.prototype.toRad = function() {
+    return this * Math.PI / 180;
+};
+
+var sendMessageToGroup = function(socket, username, head, body){
+    var user = _.findWhere(users, {username:username});
+    if(user == undefined){
+        return;
+    }
+    if(user.talkers.length == 0){
+        return;
+    }
+    _.each(user.talkers, function(membername){
+        socket.broadcast.to(membername).emit(head, body);
+    });
+};
+
+var terminateGroup = function(socket, username){
+    var user = _.findWhere(users, {username: username});
+    if(user == undefined){
+        return;
+    }
+    users = _.reject(users, {username:username});
+    _.each(user.talkers, function(talker){
+        socket.broadcast.to(talker).emit('user left', {
+            username:username,
+            numUsers: _.findWhere(users, {username:username}).talkers.length + 1
+        });
+    });
+    _.each(users, function (member){
+        member.talkers = _.reject(member.talkers, function(name){
+            return name === username;
+        });
+    });
+};
 
 exports.message = function(socket, msg) {
-    socket.broadcast.in(defaultGroupId).emit('new message', {
+    sendMessageToGroup(socket, socket.username, 'new message', {
         username: socket.username,
         message: msg
     });
 };
 
 exports.join = function(socket, username, position, radius) {
-    //TODO: group내에서 동일한 username이 있는지 확인하기(underscore.js활용), 이미 존재하면 error return하기
-    var isUnique = true;
-    if(!isUnique){
+    var isSuccessful = createTalkerGroup(socket, username, position, radius);
+    if(!isSuccessful){
         socket.emit('login failed');
         return;
     }
     socket.username = username;
-    usernames[username] = username;
-    //TODO: 사용자를 room에 넣기
-    socket.join(defaultGroupId);      //TODO: group 개념이 들어가면 사용자 이름을 넣는 것으로 바꾸라
-    // we store the username in the socket session for this client
-    socket.username = username;
-    // add the client's username to the global list
-    usernames[username] = username;
-    ++numUsers;
-    addedUser = true;
     socket.emit('login', {
-        numUsers: numUsers
+       numUsers: _.findWhere(users, {username:username}).talkers.length + 1
     });
-
-    // echo globally (all clients) that a person has connected
-    socket.broadcast.in(defaultGroupId).emit('user joined', {
+    sendMessageToGroup(socket, username, 'user joined', {
         username: socket.username,
-        numUsers: numUsers
+        numUsers: _.findWhere(users, {username: username}).talkers.length + 1
     });
 };
 
 exports.typing = function(socket) {
-    //TODO: group내 사람들에게만 보내기
-    socket.broadcast.in(defaultGroupId).emit('typing', {
+    sendMessageToGroup(socket, socket.username, 'typing', {
         username: socket.username
     });
 };
 
 exports.stoptyping = function(socket) {
-    //TODO: group내 사람들에게만 보내기
-    socket.broadcast.in(defaultGroupId).emit('stop typing', {
+    sendMessageToGroup(socket, socket.username, 'stop typing', {
         username: socket.username
     });
 };
 
 exports.disconnect = function(socket) {
-    //TODO: group에서 제거하기
-    // remove the username from global usernames list
-    if (addedUser) {
-        delete usernames[socket.username];
-        --numUsers;
-
-        // echo globally that this client has left
-        socket.broadcast.in(defaultGroupId).emit('user left', {
-            username: socket.username,
-            numUsers: numUsers
-        });
-    }
+    terminateGroup(socket, socket.username);
 };
